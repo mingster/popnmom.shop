06ead92105f38f4a2c1cf529bbf311c82e888124
mingster <mingster@users.noreply.github.com>
2024-05-07 10:34:10 +0800

    remove test data

--- pscale_dump_legod_main_20240408_103622/legod.Address.00001.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `Address`(`id`,`userId`,`firstName`,`lastName`,`company`,`countryId`,`streetLine1`,`streetLine2`,`city`,`province`,`postalCode`,`phoneNumber`,`isDefault`,`createdAt`,`updatedAt`,`reference`,`type`,`district`) VALUES
-("c17d86e5-8488-4ad9-a676-0596f020ca3e","clpeskcno000089avfp9mig0x","Ming","Tsai",NULL,"TWN","西屯區寧夏路199號6F-2","","台中市","","407","0934353651",1,"2023-11-29 02:33:15.514","2023-11-29 22:39:34.968","","HOME","西屯區");
--- pscale_dump_legod_main_20240408_103622/legod.User.00001.sql
+++ pscale_dump_legod_main_20240408_103622/legod.User.00001.sql
@@ -1,4 +1,2 @@
 INSERT INTO `User`(`id`,`name`,`email`,`emailVerified`,`image`,`createdAt`,`updatedAt`,`role`) VALUES
-("clpeskcno000089avfp9mig0x","Ming Tsai","mingster.tsai@gmail.com",NULL,"https://lh3.googleusercontent.com/a/ACg8ocJ7TUK8VpGprSX7-t2GWQCUX7XoI65PSvZ2QaoTndnLR8Q=s96-c","2023-11-26 01:18:29.653","2023-11-26 01:18:29.653","ADMIN"),
-("clr4rlges0000alanoysc8t6f","ming tsai 蔡銘升",NULL,NULL,"https://profile.line-scdn.net/0hw91JhgYqKBYMMzq8wlxXQTB2Jnt7HS5edABhIC5kI3QkAG5GMFBndy5nJiZxC2cUMlxkeC4xcHFz","2024-01-08 10:13:04.467","2024-01-08 10:13:04.467","USER"),
-("clr4rnf530003alanlz4z47ip",NULL,"ming.tsai@hotmail.com","2024-01-08 10:14:36.134",NULL,"2024-01-08 10:14:36.136","2024-01-08 10:14:36.136","USER");
+("clpeskcno000089avfp9mig0x","Ming Tsai","mingster.tsai@gmail.com",NULL,"https://lh3.googleusercontent.com/a/ACg8ocJ7TUK8VpGprSX7-t2GWQCUX7XoI65PSvZ2QaoTndnLR8Q=s96-c","2023-11-26 01:18:29.653","2023-11-26 01:18:29.653","ADMIN");
